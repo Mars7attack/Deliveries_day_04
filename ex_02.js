a66c881cbac43e9ad3a23053518a60b602c11054
Mars7attack <antoine.dufetelle@epitech.digital>
2024-10-02 11:48:10 +0200

    Create ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,18 @@
+//fonction qui ne sert pas ici
+//function miseEnPage(str) {
+//    if (str.length === 0) return ""; 
+//    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+//}
+//console.log(miseEnPage("mOnsTre"))
+
+
+function findHousing(housingToFind, housingList) {
+	let i = 0
+  while(i < housingList.lenght) {
+  	if (housingList[i].toLowerCase() === housingToFind.toLowerCase) {
+      return [i+1, fixToTheCaseSensitivity(housingList[i])]; 
+    }
+    i++
+  }
+	return null;
+}
