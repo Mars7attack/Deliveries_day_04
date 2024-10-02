ee32bcbd4cec8e621da03196168dc2759496f888
Mars7attack <antoine.dufetelle@epitech.digital>
2024-10-02 15:36:08 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,1 +1,9 @@
+function capitalizee(words) {
+  let majWord = []
+  words.forEach(words => {
+    majWord.push(words.toUpperCase())
+  })
+  return majWord
+}
 
+console.log(capitalizee(["monstre", "double monstre"]))
