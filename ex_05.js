43f9e41493843e9d785d9c47d57ac6dad8a157b5
Mars7attack <antoine.dufetelle@epitech.digital>
2024-10-02 16:37:38 +0200

    Update ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,1 +1,8 @@
-
+function getHousesPrototype() {
+  let myMap = new Map();
+  myMap.set("Apartment 75008",  90);
+  myMap.set("Apartment 94300", 105);
+  myMap.set("Apartment 92300", 140);
+  myMap.set("Apartment 75015", 65);
+  return myMap
+}
