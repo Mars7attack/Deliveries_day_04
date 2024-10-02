3c8524fef8e5f2fabd247205f1fc035416c81d74
Mars7attack <antoine.dufetelle@epitech.digital>
2024-10-02 11:52:39 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,16 +1,15 @@
-//fonction qui ne sert pas ici
-//function miseEnPage(str) {
-//    if (str.length === 0) return ""; 
-//    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-//}
-//console.log(miseEnPage("mOnsTre"))
+function miseEnPage(str) {
+    if (str.length === 0) return ""; 
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+console.log(miseEnPage("mOnsTre"))
 
 
 function findHousing(housingToFind, housingList) {
 	let i = 0
   while(i < housingList.lenght) {
   	if (housingList[i].toLowerCase() === housingToFind.toLowerCase) {
-      return [i+1, fixToTheCaseSensitivity(housingList[i])]; 
+      return [i+1, miseEnPage(housingList[i])]; 
     }
     i++
   }
